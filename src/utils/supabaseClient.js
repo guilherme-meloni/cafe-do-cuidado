9bc69b53ce29c3c525acaba7b626e2f608febd25
Guilherme <51010707+guilherme-meloni@users.noreply.github.com>
2025-06-14 16:32:41 -0300

    Melhora verificação de variáveis de ambiente

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,13 @@
 // src/utils/supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+// Esta verificação vai nos dar um erro muito mais claro se as chaves não forem encontradas
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("URL ou Chave Anon do Supabase não encontradas. Verifique as variáveis de ambiente.")
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
